f7af644f0a4ffdb1c5a64c907feac97c60a3bec2
zer0tres <109388900+zer0tres@users.noreply.github.com>
2026-01-31 14:25:09 +0000

    feat: adicionar modelo Customer ao módulo de modelos

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,4 +1,5 @@
 from app.models.user import User
 from app.models.company import Company
+from app.models.customer import Customer
 
-__all__ = ['User', 'Company']
+__all__ = ['User', 'Company', 'Customer']
