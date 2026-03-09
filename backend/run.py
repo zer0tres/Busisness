9ab3e7fbd52bc80548631c5cff2171eb1fda6d97
zer0tres <109388900+zer0tres@users.noreply.github.com>
2026-03-09 22:09:49 +0000

    fix: rodar migrations automaticamente no startup

--- backend/run.py
+++ backend/run.py
@@ -1,10 +1,11 @@
 from app import create_app, db, register_frontend
+from flask_migrate import upgrade
 
 app = create_app()
 register_frontend(app)
 
 with app.app_context():
-    db.create_all()
+    upgrade()  # roda todas as migrations pendentes automaticamente
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
